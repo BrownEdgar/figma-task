464c78298d345b17c92ed5e23086eb188a63b2f6
Hayk <hayk.voskanyan96@gmail.com>
2024-04-11 11:57:01 +0400

    A

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,5 @@ import React from "react";
 import "./App.scss";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div>
-        <img src="./image1.svg" alt="" />
-      </div>
-      <div className="App__items">
-        <h1>Get the app!</h1>
-        <h5>
-          Thanks to our detailed search, you can easily meet the right person.
-          Explore our custom selection of detailed dating profiles.
-        </h5>
-        <img src="./image2.svg" alt="" />
-        <div className="App__buttons">
-          <button>
-            <p>Upload to</p>
-            <i class="bi bi-apple"></i>
-            <span>App Store</span>
-          </button>
-          <button>
-            <p>Available in</p>
-            <p>
-              <img className="google" src="./logo.svg" alt="" />
-              <span>Google Play</span>
-            </p>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+<h1>Hayk</h1>
 }
