0aef9347d1bc7b66c38f7f41495d582d847be374
Hayk <hayk.voskanyan96@gmail.com>
2024-04-11 12:04:48 +0400

    B

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./App.scss";
 
 export default function App() {
-<h1>Hayk</h1>
+    return (
+        <div>
+            <h1>Hayk</h1>
+            <h2>Voskanyan</h2>
+        </div>
+    )
+
 }
