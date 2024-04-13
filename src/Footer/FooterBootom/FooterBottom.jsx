2a43ae47f127202fe11844854ffa97af41749268
Hayk <hayk.voskanyan96@gmail.com>
2024-04-13 10:34:09 +0400

    Footer-bottom

---
+++ src/Footer/FooterBootom/FooterBottom.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FooterBottom.scss'
+
+export default function FooterBottom() {
+  return (
+    <div className='FooterBottom'>
+        <p>© 2023 <span>NextLove®</span> All Rights Reserved. </p>
+    </div>
+  )
+}
--- src/Footer/FooterReGroup/FooterGroup.jsx
+++ src/Footer/FooterReGroup/FooterGroup.jsx
@@ -3,14 +3,18 @@ import SectionN1 from "../FooterLeft/SectionN1";
 import SectionN2 from "../FooterCenter/SectionN2";
 import SectionN3 from"../FooterRight/SectionN3";
 import './FooterGroup.scss'
+import FooterBottom from '../FooterBootom/FooterBottom';
 
 export default function FooterGroup() {
   return (
+    <>
     <footer className="Footer">
     <SectionN1/>
     <SectionN2/>
     <SectionN3/>
   </footer>
+   <FooterBottom/>
+   </>
   )
 }
 
