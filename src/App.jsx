5a4b029417b0aa629be26294978ae5bee1b43e55
gevv177 <gevhakobyan177@gmail.com>
2024-04-14 20:00:19 +0400

    NextLove page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.scss'
+import NextLove from './components/NextLove/NextLove'
 
 export default function App() {
   return (
     <div className='App'>
-
+      <NextLove />
     </div>
   )
 }
