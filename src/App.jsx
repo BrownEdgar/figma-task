92b05e5ea8c4ff25a3706d175dbf5e5b6a2b9cef
gevv177 <gevhakobyan177@gmail.com>
2024-04-17 10:20:43 +0400

    questions section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.scss";
 import FooterGroup from "./components/Footer/FooterReGroup/FooterGroup";
 import Application from './components/Application/Application';
 import NextLove from './components/NextLove/NextLove'
+import Questions from "./components/Questions/Questions";
 
 export default function App() {
   return (
     <div className="App">
       <NextLove />
       <Application />
+      <Questions />
       <FooterGroup />
     </div>
   )
